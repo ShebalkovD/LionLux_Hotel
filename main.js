71bae276d5055aad3fac7eb032b4c64c138a913c
ShebalkovD <ShebalkovDenis.wk@gmail.com>
2023-07-07 15:05:17 +0300

    Меню услуг

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Паралакс анимация шапки
+
 const nav = document.querySelector('.nav');
 const scrollTrigger = document.querySelector('.scrolltrigger');
 const options={
@@ -14,3 +16,25 @@ const navObserver = new IntersectionObserver((entries) => {
 }, options)
 
 navObserver.observe(scrollTrigger)
+
+//Меню услуг
+
+const btnServices = document.querySelectorAll('.btn_services');
+const menuServices = document.querySelector('.services_menu');
+const shadow = document.querySelector('.shadow');
+const body = document.querySelector('body');
+
+
+btnServices.forEach(el => {
+    el.onclick = () => {
+        menuServices.classList.toggle('services_menu-active');
+        shadow.classList.toggle('shadow-active');
+        body.classList.toggle('noscroll');
+    }
+})
+
+shadow.onclick = () => {
+    menuServices.classList.remove('services_menu-active');
+    shadow.classList.remove('shadow-active');
+    body.classList.remove('noscroll');
+}
